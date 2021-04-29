b210f36102f1e9973f85c9a8cb2e0753a5519960
LittleFootDev <thelittlefootdev@gmail.com>
2021-04-29 19:20:39 +0200

    ajout de la fonction de trie part date

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import "./index.scss";
 
 const articleContainerElement = document.querySelector(".articles-container");
 const categoriesContainerElement =  document.querySelector('.categories');
+const selectElement = document.querySelector('select');
 let filter; 
 let articles;
+let sortBy ='desc'
+
+selectElement.addEventListener('change', () => {
+  sortBy = selectElement.value;
+
+  fetchArticle();
+
+});
 
 const createArticles = () => {
 
@@ -71,7 +80,10 @@ const createArticles = () => {
 const displayMenuCategories = categoriesArr => {
   const liElements = categoriesArr.map( categoryElem => {
     const li = document.createElement('li');
-    li.innerHTML = `<li>${categoryElem[0]} (<strong>${categoryElem[1]} </strong>)</li>`;
+    li.innerHTML = `${categoryElem[0]} (<strong>${categoryElem[1]} </strong>)`;
+    if(categoryElem[0] === filter) {
+      li.classList.add('active');
+    }
     li.addEventListener('click', () => {
       if (filter === categoryElem[0]) {
         filter = null;
@@ -111,7 +123,7 @@ const createMenuCategories = () => {
 
 const fetchArticle = async () => {
   try {
-    const response = await fetch("https://restapi.fr/api/littlefoots");
+    const response = await fetch(`https://restapi.fr/api/littlefoots?sort=createAt:${sortBy}`);
     articles = await response.json();
     createArticles();
     createMenuCategories();
