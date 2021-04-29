36b9ab051e4dae79207e6b25edbb4e3bc17e2f7a
LittleFootDev <thelittlefootdev@gmail.com>
2021-04-29 18:45:51 +0200

    correction d'erreur scss, plus ajout de la fonction filter sur le menue catégories

--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ import "./index.scss";
 
 const articleContainerElement = document.querySelector(".articles-container");
 const categoriesContainerElement =  document.querySelector('.categories');
+let filter; 
+let articles;
 
-const createArticles = articles => {
-  const articlesDOM = articles.map(article => {
+const createArticles = () => {
+
+  const articlesDOM = articles.filter( (article) => {
+    if (filter) {
+      return article.category === filter
+    }else {
+      return true;
+    }
+  }).map(article => {
     const articleDOM = document.createElement("div");
     articleDOM.classList.add("article");
     articleDOM.innerHTML = `
@@ -63,6 +72,20 @@ const displayMenuCategories = categoriesArr => {
   const liElements = categoriesArr.map( categoryElem => {
     const li = document.createElement('li');
     li.innerHTML = `<li>${categoryElem[0]} (<strong>${categoryElem[1]} </strong>)</li>`;
+    li.addEventListener('click', () => {
+      if (filter === categoryElem[0]) {
+        filter = null;
+        li.classList.remove('active');
+        createArticles();
+      } else {
+        filter = categoryElem[0];
+        liElements.forEach( li => {
+          li.classList.remove('active')
+        })
+        li.classList.add('active');
+        createArticles();
+      }
+    })
     return li;
   });
   categoriesContainerElement.innerHTML = '';
@@ -70,7 +93,7 @@ const displayMenuCategories = categoriesArr => {
   console.log(liElements);
 }; 
 
-const createMenuCategories = articles => {
+const createMenuCategories = () => {
   const categories = articles.reduce((acc, article) => {
     if(acc[article.category]){
       acc[article.category]++;
@@ -82,16 +105,16 @@ const createMenuCategories = articles => {
 
   const categoriesArr = Object.keys(categories).map( category => {
     return [category, categories[category]];
-  });
+  }).sort( (c1, c2 ) => c1[0].localeCompare(c2[0]));
   displayMenuCategories(categoriesArr)
 }
 
 const fetchArticle = async () => {
   try {
     const response = await fetch("https://restapi.fr/api/littlefoots");
-    const articles = await response.json();
-    createArticles(articles);
-    createMenuCategories(articles);
+    articles = await response.json();
+    createArticles();
+    createMenuCategories();
   } catch (e) {
     console.log("e : ", e);
   }
